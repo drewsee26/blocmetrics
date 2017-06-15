0d0d94984e8cc2b1cbd6af9a3a53cbccb78b9e68
Drew Hollenback <drewsee26@gmail.com>
2017-06-15 17:50:19 +0000

    CP 6 code review

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
     resources :registered_applications do
     end
     
+    
+    namespace :api, defaults: { format: :json } do
+        match '/events', to: 'events#preflight', via: [:options]
+        resources :events, only: [:create]
+    end
+    
     root 'welcome#index' 
 end
