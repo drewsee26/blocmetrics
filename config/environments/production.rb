0ea8f58100a215c2fe9c258b9cb35ed9c22e4af0
Drew Hollenback <drewsee26@gmail.com>
2017-07-07 22:42:53 +0000

    update prod action mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://glacial-dusk-89418.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://drews-blocmetrics.herokuapp.com/' }
 end
